b0d796852cac3fd9240337728da35bf9cb2807df
v.nam <v4lentine.nam@gmail.com>
2024-03-04 19:30:18 +0300

    Task 2

--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@ const task1 = (arr) => {
     arr.forEach(element => {
         console.log(`Привет, ${element}`);
     });
-}
+};
 
 const t1Array = ['Иван', 'Мария', 'Алексей', 'Ольга', 'Сергей', 'Анна'];
 
 console.log('Task 1');
 task1(t1Array);
+
+const task2 = (arr) => {
+    return arr.map(element => element * 10);
+};
+
+const t2Array = [1, 2, 3, 4, 5];
+const t2ModifiedArray = task2(t2Array);
+
+console.log('Task 2');
+console.log(`Array:`);
+console.log(t2Array);
+console.log(`Modified array:`);
+console.log(t2ModifiedArray);
